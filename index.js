88bda55389b376af0fd40883176d5bd4ff0bdf06
smithspencer817 <smithspencer817@gmail.com>
2020-11-17 11:49:51 -0600

    syncing heroku app to DB

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 
+// HEROKU DATABASE
+const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
 // MIDDLEWARE
 app.use(cors());
 app.use(express.json());
@@ -14,6 +23,20 @@ app.use('/api/login', require('./routes/Login'));
 app.use('/api/users', require('./routes/UsersController'));
 app.use('/api/lists', require('./routes/ListsController'));
 app.use('/api/list-items', require('./routes/ListItemsController'));
+
+// HEROKU DATABASE
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+})
 
 // SERVER
 app.listen(PORT, () => {
